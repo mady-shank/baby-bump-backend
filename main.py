04a3d2bb7b8169b6d51c2410ebf83fe2ed1c17f1
mady-shank <67336447+mady-shank@users.noreply.github.com>
2020-06-29 12:06:25 -0400

    Creating Templates
    Created templates, set up basic structure

--- main.py
+++ main.py
@@ -1,11 +1,19 @@
-from flask import Flask
+from flask import Flask, render_template, url_for
 app = Flask(__name__)
 
 @app.route('/')
 def root():
-	return "<h1>Hello World!</h1>"
+	return render_template('home.html')
+
+@app.route('/login')
+def login():
+    return render_template('login.html', title='Login')
+
+@app.route('/register')
+def register():
+    return render_template('register.html', title='Register')
 
 
 # start the flask app
 if __name__ == '__main__':
-	app.run()
+	app.run(debug=True)
