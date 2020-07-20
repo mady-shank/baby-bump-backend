0bb6a1a6c2ed6ea7400dab5edfff87a758656048
mady-shank <67336447+mady-shank@users.noreply.github.com>
2020-07-20 11:02:44 -0400

    Taking DB back out

--- database.py
+++ database.py
@@ -1,4 +1,4 @@
-import mysql.connector
+#import mysql.connector
 import os
 from os import getenv
 from dotenv import load_dotenv
@@ -10,18 +10,18 @@ load_dotenv()
 password = os.getenv("password_db")
 
 #connect to server
-db = mysql.connector.connect(
-    host = "127.0.0.1",
-    #port = "5000",
-    #host = "localhost",
-    user = "root",
-    passwd = password,
-    database = "babybump"
+#db = mysql.connector.connect(
+#    host = "127.0.0.1",
+#    #port = "5000",
+#    #host = "localhost",
+#    user = "root",
+#    passwd = password,
+#    database = "babybump"
 )
 
 #set cursor object
-mycursor = db.cursor()
+#mycursor = db.cursor()
 
 # create database and tables if they don't already exist
-mycursor.execute("CREATE DATABASE IF NOT EXISTS babybump DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci;".format("babybump"));
-mycursor.execute("CREATE TABLE IF NOT EXISTS User (username VARCHAR(25), password VARCHAR(25), email VARCHAR(25), role VARCHAR(25), id int PRIMARY KEY AUTO_INCREMENT)")
+#mycursor.execute("CREATE DATABASE IF NOT EXISTS babybump DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci;".format("babybump"));
+#mycursor.execute("CREATE TABLE IF NOT EXISTS User (username VARCHAR(25), password VARCHAR(25), email VARCHAR(25), role VARCHAR(25), id int PRIMARY KEY AUTO_INCREMENT)")
--- main.py
+++ main.py
@@ -1,11 +1,11 @@
 from flask import Flask, request, session, redirect, url_for, flash
 from flask_login import logout_user
-import mysql.connector
+#import mysql.connector
 import os
 from os import getenv
 from dotenv import load_dotenv
-import database
-from database import mycursor
+#import database
+#from database import mycursor
 from flask_session import Session
 
 app = Flask(__name__,
@@ -33,20 +33,20 @@ def register():
     Role = request.args.get("role")
 
     #if this returns a user, then the email already exists in the database
-    mycursor.execute('SELECT email from User WHERE email = %(email)s', {'email' : Email })
-    checkUser = mycursor.fetchall()
+    #mycursor.execute('SELECT email from User WHERE email = %(email)s', {'email' : Email })
+    #checkUser = mycursor.fetchall()
 
     # if a user is returned, the user is rerouted to the register page, where they can try a different email.
-    if checkUser:
-        flash('This email has already been registered.')
-        return redirect(url_for('register'))
+    #if checkUser:
+    #    flash('This email has already been registered.')
+    #    return redirect(url_for('register'))
 
     #add user info to the table User
-    add_user = "INSERT INTO User (username, password, email, role) VALUES (%s, %s, %s, %s)"
-    user_info = (Username, Password, Email, Role)
-    database.mycursor.execute(add_user, user_info)
-    database.db.commit()
-    database.db.close()
+    #add_user = "INSERT INTO User (username, password, email, role) VALUES (%s, %s, %s, %s)"
+    #user_info = (Username, Password, Email, Role)
+    #database.mycursor.execute(add_user, user_info)
+    #database.db.commit()
+    #database.db.close()
 
     return "Registration Complete"
 
@@ -56,9 +56,9 @@ def login():
     username = request.form.get('username')
     password = request.form.get('password')
 
-    mycursor.execute('SELECT * FROM User WHERE username = %s and password = %s', (username, password))
-    account = mycursor.fetchone()
-    database.db.close()
+    #mycursor.execute('SELECT * FROM User WHERE username = %s and password = %s', (username, password))
+    #account = mycursor.fetchone()
+    #database.db.close()
 
     if account:
         session['username'] = session.form('username')
