7f2945b65b2dd77124253084ce19a2952cdd64ce
mady-shank <67336447+mady-shank@users.noreply.github.com>
2020-07-20 07:43:08 -0400

    Adding database stuff back in

--- database.py
+++ database.py
@@ -9,16 +9,16 @@ load_dotenv()
 password = os.getenv("password_db")
 
 #connect to server
-#db = mysql.connector.connect(
-#    host = "localhost",
-#    user = "root",
-#    passwd = password,
-#    database = "babybump"
-#)
+db = mysql.connector.connect(
+    host = "localhost",
+    user = "root",
+    passwd = password,
+    database = "babybump"
+)
 
 #set cursor object
-#mycursor = db.cursor()
+mycursor = db.cursor()
 
 # create database and tables if they don't already exist
-#mycursor.execute("CREATE DATABASE IF NOT EXISTS babybump DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci;".format("babybump"));
-#mycursor.execute("CREATE TABLE IF NOT EXISTS User (username VARCHAR(25), password VARCHAR(25), email VARCHAR(25), role VARCHAR(25), id int PRIMARY KEY AUTO_INCREMENT)")
+mycursor.execute("CREATE DATABASE IF NOT EXISTS babybump DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci;".format("babybump"));
+mycursor.execute("CREATE TABLE IF NOT EXISTS User (username VARCHAR(25), password VARCHAR(25), email VARCHAR(25), role VARCHAR(25), id int PRIMARY KEY AUTO_INCREMENT)")
